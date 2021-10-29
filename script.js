23150e51e3a85bb85c8c567ba572cfa6ad9dabb1
RayPas <70732899+RayPas@users.noreply.github.com>
2021-10-28 23:14:42 -0700

    Style and script adjustments
    corrected small errors in the script and also added the correct answer.
    
    Created a CSS to kind of design the button around the questions.

--- script.js
+++ script.js
@@ -4,30 +4,35 @@ const quizData =[
         a: 'Black Widow',
         b: 'Captain America',
         c: 'Iron Man',
-        d: 'Hulk'
+        d: 'Hulk',
+        correct: 'c'
     } , {
         question: 'What is the best anime?',
         a: 'One Punch Man',
         b: 'Overlord',
         c: 'Avatar',
-        d: 'DBZ'
+        d: 'DBZ',
+        correct: 'b'
     } , {
         question: 'What is the hardest bouldering grade currently?',
         a: 'V17',
         b: 'V16',
         c: 'V18',
-        d: 'V20'
+        d: 'V20',
+        correct: 'b'
     } , {
         question: 'Where did the last botulism in America happen?',
         a: 'McDonalds',
         b: 'SafeWay',
         c: '7-Eleven',
-        d: 'WalMart'
+        d: 'WalMart',
+        correct: 'c'
     } , {
         question: 'What does the F in FAANG stand for??',
         a: 'Facebook',
         b: 'Flickr',
         c: 'Friday',
-        d: 'Meta'
+        d: 'Meta',
+        correct: 'd'
     } 
 ]
