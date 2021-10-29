b173c4f0bc19337c7c6f05d6b72162e2ff0656b2
RayPas <70732899+RayPas@users.noreply.github.com>
2021-10-28 19:52:17 -0700

    Question Object literal
    Emmeret, and created object literals for the questions.

---
+++ script.js
@@ -0,0 +1,33 @@
+const quizData =[
+    {
+        question: 'Who is the best avenger?',
+        a: 'Black Widow',
+        b: 'Captain America',
+        c: 'Iron Man',
+        d: 'Hulk'
+    } , {
+        question: 'What is the best anime?',
+        a: 'One Punch Man',
+        b: 'Overlord',
+        c: 'Avatar',
+        d: 'DBZ'
+    } , {
+        question: 'What is the hardest bouldering grade currently?',
+        a: 'V17',
+        b: 'V16',
+        c: 'V18',
+        d: 'V20'
+    } , {
+        question: 'Where did the last botulism in America happen?',
+        a: 'McDonalds',
+        b: 'SafeWay',
+        c: '7-Eleven',
+        d: 'WalMart'
+    } , {
+        question: 'What does the F in FAANG stand for??',
+        a: 'Facebook',
+        b: 'Flickr',
+        c: 'Friday',
+        d: 'Meta'
+    } 
+]
